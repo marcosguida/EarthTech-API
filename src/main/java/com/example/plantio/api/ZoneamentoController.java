4ee526c46b97e3480ef02f2d9b15145018ea7b2b
marcos <ribeiroguidam@gmail.com>
2024-04-16 22:32:47 -0300

    Criar tabelas no Mysql e Injeção de Dependência
    - Criei a tabela zoneamento para armazenar os campos: id, safra, cultura, solo, municipio, microrregião.
    - Testei inicialização do banco de dados.
    - Inseri dependências do JPA no pom.xml
    - Inseri a anotação de serviço de Injeção de Dependência (@Autowired) na classe ZoneamentoController
    
    Close #01

--- src/main/java/com/example/plantio/api/ZoneamentoController.java
+++ src/main/java/com/example/plantio/api/ZoneamentoController.java
@@ -2,6 +2,7 @@ package com.example.plantio.api;
 
 import com.example.plantio.model.Zoneamento;
 import com.example.plantio.service.Zoneamentoservice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/zoneamento")
 public class ZoneamentoController {
-    private Zoneamentoservice service = new Zoneamentoservice();
+    @Autowired
+    private Zoneamentoservice service;
 
     @GetMapping
     public List<Zoneamento> get(){
--- src/main/java/com/example/plantio/service/Zoneamentoservice.java
+++ src/main/java/com/example/plantio/service/Zoneamentoservice.java
@@ -1,18 +1,16 @@
 package com.example.plantio.service;
 
 import com.example.plantio.model.Zoneamento;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class Zoneamentoservice {
 
     public List<Zoneamento> getZoneamento(){
         List<Zoneamento> zoneamentos = new ArrayList<>();
-
-        zoneamentos.add(new Zoneamento(1L, "2023/2024", "Milho", "AD1", "Palmas"));
-        zoneamentos.add(new Zoneamento(2L, "2022/2023", "Soja", "AD2", "Paraiso"));
-        zoneamentos.add(new Zoneamento(3L, "2021/2022", "Feijão", "AD1", "Porto Nacional"));
         return zoneamentos;
     }
 }
