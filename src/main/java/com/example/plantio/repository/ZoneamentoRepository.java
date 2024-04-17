238bc529ff5ceab02688f2480369cddd5ada3b7b
marcos <ribeiroguidam@gmail.com>
2024-04-17 16:56:30 -0300

    Mapeamento do banco de dados e repository
    - Inserir anotações para persistência de dados e geração automática de ID's. (@Entity e @Id )
    - Mapeação de tabela 'zoneamento' do banco de dados.
    - Criei classe ZoneamentoRepository.
    
    Close #3

---
+++ src/main/java/com/example/plantio/repository/ZoneamentoRepository.java
@@ -0,0 +1,8 @@
+package com.example.plantio.repository;
+
+import com.example.plantio.model.Zoneamento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ZoneamentoRepository extends CrudRepository<Zoneamento, Long> {
+
+}
