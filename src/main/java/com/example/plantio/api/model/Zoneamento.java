7c5f0df1a4d161bec4f6ab4b6a8dd577be6bf5e0
marcos <ribeiroguidam@gmail.com>
2024-04-30 21:51:19 -0300

    Tratamento de erros
    - Inseri as dependências de segurança do Spring Boot no arquivo pom.xml;
    - Desabilitei o campo de eclusão de autoconfiguração de segurança da classe de controller: @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class }) ;
    - Inseri o spring.security.user.name=user e spring.security.user.password=senha no arquivo application.properties;
    - Configurei o Basic Auth no Postman.
    
    Close #20

--- src/main/java/com/example/plantio/api/model/Zoneamento.java
+++ src/main/java/com/example/plantio/api/model/Zoneamento.java
@@ -4,14 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * @author Marcos Ribeiro
  */
 
-@Data
 @Entity
 public class Zoneamento {
 
@@ -85,4 +82,3 @@ public class Zoneamento {
     }
 
 }
-
