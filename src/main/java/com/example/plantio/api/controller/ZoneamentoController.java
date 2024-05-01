478bce4fb461db423ffcd5f79276df7cae10f8b1
marcos <ribeiroguidam@gmail.com>
2024-05-01 00:43:51 -0300

    Close #22

--- src/main/java/com/example/plantio/api/controller/ZoneamentoController.java
+++ src/main/java/com/example/plantio/api/controller/ZoneamentoController.java
@@ -20,6 +20,8 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/zoneamento")
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
+
 public class ZoneamentoController {
     @Autowired
     private ZoneamentoService service;
