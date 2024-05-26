080c539458ef5d968ce98e2552310bf889ab20bd
Marcos Guida <105091587+marcosguida@users.noreply.github.com>
2024-05-26 11:07:22 -0300

    Create import-drenagem.sql

---
+++ import-drenagem.sql
@@ -0,0 +1,16 @@
+use earthTech_db;
+INSERT INTO medias (classe,areia,argila,silte,dtype) VALUES 
+('Vertissolo',228.06,488.97,282.98,'drenagem'),
+('Plintossolo',386.18,323.68,292.58,'drenagem'),
+('Planossolo',572.32,220.77,206.84,'drenagem'),
+('Organossolo',360.64,315.58,316.77,'drenagem'),
+('Não há dados',254.23,537.14,208.63,'drenagem'),
+('Nitossolo',245.05,518.51,235.01,'drenagem'),
+('Neossolo',635.82,169.68,194.15,'drenagem'),
+('Luvissolo',413.9,324.99,261.5,'drenagem'),
+('Latossolo',395.32,483.63,119.39,'drenagem'),
+('Gleissolo',289.43,406.81,305.66,'drenagem'),
+('Espodossolo',855.33,59.77,85.54,'drenagem'),
+('Chernossolo',337.17,366.28,296.4,'drenagem'),
+('Cambissolo',337.46,370.85,290.46,'drenagem'),
+('Argissolo',464.96,340.03,193.94,'drenagem');
